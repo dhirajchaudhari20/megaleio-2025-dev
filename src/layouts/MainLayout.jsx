9e53e635ace9cc3e0da83c8bc99886c5302f31ab
Dhiraj Chaudhari <dhirajchaudhari@Dhirajs-MacBook-Air.local>
2026-03-06 19:33:46 +0530

    chore: remove flashlight cursor and vhs overlay, restore visibility

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,15 +1,11 @@
 import Navbar from "../components/Navbar";
 import { Outlet } from "react-router-dom";
 import Particles from "../components/Effects/Particles";
-import CustomCursor from "../components/Effects/CustomCursor";
 import AudioToggle from "../components/Effects/AudioToggle";
-import VHSGlitch from "../components/Effects/VHSGlitch";
 
 const MainLayout = () => {
   return (
     <>
-      <VHSGlitch />
-      <CustomCursor />
       <Particles />
       <Navbar />
       <Outlet />
